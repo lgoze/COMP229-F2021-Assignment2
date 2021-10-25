880b65f757e2f1ee6d97bfcb120301bfbf23428c
lgoze <lgoze@my.centennialcollege.ca>
2021-10-24 21:31:51 -0400

    Rearranged folders

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,25 +3,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.DisplayContactPage = exports.DisplayServicesPage = exports.DisplayProjectsPage = exports.DisplayAboutPage = exports.DisplayHomePage = void 0;
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
-router.get('/', function (req, res, next) {
+function DisplayHomePage(req, res, next) {
     res.render('index', { title: 'Home', content: 'home_content' });
-});
-router.get('/home', function (req, res, next) {
-    res.render('index', { title: 'Home', content: 'home_content' });
-});
-router.get('/about', function (req, res, next) {
+}
+exports.DisplayHomePage = DisplayHomePage;
+function DisplayAboutPage(req, res, next) {
     res.render('index', { title: 'About Me', content: 'about_content' });
-});
-router.get('/projects', function (req, res, next) {
+}
+exports.DisplayAboutPage = DisplayAboutPage;
+function DisplayProjectsPage(req, res, next) {
     res.render('index', { title: 'Projects', content: 'projects_content' });
-});
-router.get('/services', function (req, res, next) {
+}
+exports.DisplayProjectsPage = DisplayProjectsPage;
+function DisplayServicesPage(req, res, next) {
     res.render('index', { title: 'Services', content: 'services_content' });
-});
-router.get('/contact', function (req, res, next) {
+}
+exports.DisplayServicesPage = DisplayServicesPage;
+function DisplayContactPage(req, res, next) {
     res.render('index', { title: 'Contact Me', content: 'contact_content' });
-});
-exports.default = router;
+}
+exports.DisplayContactPage = DisplayContactPage;
 //# sourceMappingURL=index.js.map
